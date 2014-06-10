bfaf450fced21c0c0f3fa18436889028aecfcc6d
GHC124 <chung.pv0795@gmail.com>
2014-06-10 17:27:05 +0700

    - Update chooseFileDialog

--- EDashboard/src/main/webapp/WEB-INF/tags/html/chooseFile.tag
+++ EDashboard/src/main/webapp/WEB-INF/tags/html/chooseFile.tag
@@ -1,16 +1,6 @@
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
 
-<spring:message code="label.actions" var="labelActions"/>
-<spring:message code="label.action.choose" var="labelActionChoose"/>
-<spring:message code="label.content.file.folder.name" var="labelFolderName"/>
-<spring:message code="label.content.file.folder.description" var="labelFolderDescription"/>
-<spring:message code="label.content.file.name" var="labelFileName"/>
-<spring:message code="label.content.file.description" var="labelFileDescription"/>
-<spring:message code="label.content.file.size" var="labelFileSize"/>
-<spring:message code="label.content.file.date_up" var="labelFileDateUp"/>
-<spring:message code="label.action.save" var="labelActionSave"/>
-<spring:message code="label.action.close" var="labelActionClose"/>
-<spring:url value="/secured/content/file" var="showFileUrl"/>	
+<spring:message code="label.action.choose" var="labelActionChoose"/>	
 
 <div id="chooseFileDialog" title="${labelActionChoose}" class="dialog choose-dialog" style="display: none">		
 	<div>
--- EDashboard/src/main/webapp/WEB-INF/web-resources/scripts/chooseFile.js
+++ EDashboard/src/main/webapp/WEB-INF/web-resources/scripts/chooseFile.js
@@ -1,94 +1,118 @@
+function chooseFileDialog(){
+	this.showFileUrl='';
+	this.labelFolderName='';
+	this.labelFolderDescription='';
+	this.labelActions='';
+	this.labelFileName='';
+	this.labelFileDescription='';
+	this.labelFileSize='';
+	this.labelFileDateUp='';
+	this.labelActionSave='';
+	this.labelActionClose='';
+	this.globalSelectedFolderId='';
+	this.listFolderId='';
+	this.listFileId='';
+	this.pagerFolderId='';
+	this.pagerFileId='';	
+	this.initLabel = function(){
+		
+	};
+	this.initStructure = function(){
+		var current = this;
+		$("#" + this.listFolderId).jqGrid({ 
+		    url: current.showFileUrl + '/folders', 
+		    datatype: 'local', 
+		    mtype: 'GET', 
+		    colNames:[ current.labelFolderName, current.labelFolderDescription, current.labelActions], 
+		    colModel :[  
+		      {name:'name', index:'name', width:150},
+		      {name:'description', index:'description', sortable:false, width:150},
+		      {name:'act', index:'act', sortable:false, width:25}
+		    ], 
+		    jsonReader : { 
+		        root:"data", 
+		        page: "currentPage", 
+		        total: "totalPages", 
+		        records: "totalRecords", 
+		        repeatitems: false, 
+		        id: "id" 
+		    },       
+		    pager: '#' + current.pagerFolderId, 
+		    rowNum:5, 
+		    rowList:[5,10,20,30,50], 
+		    sortname: 'name', 
+		    sortorder: 'asc', 
+		    viewrecords: true, 
+		    gridview: true, 
+		    height: 150, 
+		    width: 700, 
+		    beforeRequest: function(){
+		    	
+		    	//clearJQGridData($('#listFile'), {page : 1});
+		    },
+		    onSelectRow: function(id){ 
+		    	globalSelectedFolderId = id;		    	
+		    	refreshJQGrid('#' + current.listFileId, { page: 1, postData: {folderId:id}, datatype: "json" });
+		    },
+		    loadComplete: function() {	
+		    	var grid = $(this);
+		        
+		    }
+		  });	    	
+		$("#" + this.listFileId).jqGrid({ 
+		  url: current.showFileUrl + '/listFile', 
+		  datatype: 'local', 
+		  mtype: 'GET', 
+		  colNames:[ current.labelFileName, current.labelFileDescription, current.labelFileSize, current.labelFileDateUp], 
+		  colModel :[  
+		    {name:'name', index:'name', width:150},
+		    {name:'description', index:'description', sortable:false, width:150},
+		    {name:'sizeString', index:'size', width:50},
+		    {name:'dateUpString', index:'dateUp', width:50}
+		  ], 
+		  jsonReader : { 
+		      root:"data", 
+		      page: "currentPage", 
+		      total: "totalPages", 
+		      records: "totalRecords", 
+		      repeatitems: false, 
+		      id: "id" 
+		  },       
+		  pager: '#' + current.pagerFileId, 
+		  rowNum:5, 
+		  rowList:[5,10,20,30,50], 
+		  sortname: 'name', 
+		  sortorder: 'asc', 
+		  viewrecords: true, 
+		  gridview: true, 
+		  height: 250, 
+		  width: 700, 
+		  onSelectRow: function(id){  
+		      //document.location.href ="${showFolderUrl}/" + id; 
+		  },
+		  loadComplete: function() {			    	
+		  }
+		});		
+		$( "#chooseFileDialog" ).dialog({
+	          autoOpen: false,
+	          height: 400,
+	          width: 500,
+	          modal: true,
+	          resizable: false,
+	          buttons:{
+	        		 '${labelActionSave}':function(){
+	        			
+	        		},
+	        		'${labelActionClose}':function(){
+	        			$("#chooseFileDialog").dialog("close");	
+	        		}  
+	          }
+	    });
+	};
+}
 var globalSelectedFolderId = -1;            
 $(function(){       
-	$("#listFolder").jqGrid({ 
-	    url:'${showFileUrl}/folders', 
-	    datatype: 'local', 
-	    mtype: 'GET', 
-	    colNames:['${labelFolderName}', '${labelFolderDescription}', '${labelActions}'], 
-	    colModel :[  
-	      {name:'name', index:'name', width:150},
-	      {name:'description', index:'description', sortable:false, width:150},
-	      {name:'act', index:'act', sortable:false, width:25}
-	    ], 
-	    jsonReader : { 
-	        root:"data", 
-	        page: "currentPage", 
-	        total: "totalPages", 
-	        records: "totalRecords", 
-	        repeatitems: false, 
-	        id: "id" 
-	    },       
-	    pager: '#pagerFolder', 
-	    rowNum:5, 
-	    rowList:[5,10,20,30,50], 
-	    sortname: 'name', 
-	    sortorder: 'asc', 
-	    viewrecords: true, 
-	    gridview: true, 
-	    height: 150, 
-	    width: 700, 
-	    beforeRequest: function(){
-	    	disableInput($('#btnNewFile'));
-	    	//clearJQGridData($('#listFile'), {page : 1});
-	    },
-	    onSelectRow: function(id){ 
-	    	globalSelectedFolderId = id;		    	
-	    	refreshJQGrid('#listFile', { page: 1, postData: {folderId:id}, datatype: "json" });
-	    },
-	    loadComplete: function() {	
-	    	var grid = $(this);
-	        
-	    }
-	  });	    	
-	$("#listFile").jqGrid({ 
-	  url:'${showFileUrl}/listFile', 
-	  datatype: 'local', 
-	  mtype: 'GET', 
-	  colNames:['${labelFileName}', '${labelFileDescription}', '${labelFileSize}', '${labelFileDateUp}'], 
-	  colModel :[  
-	    {name:'name', index:'name', width:150},
-	    {name:'description', index:'description', sortable:false, width:150},
-	    {name:'sizeString', index:'size', width:50},
-	    {name:'dateUpString', index:'dateUp', width:50}
-	  ], 
-	  jsonReader : { 
-	      root:"data", 
-	      page: "currentPage", 
-	      total: "totalPages", 
-	      records: "totalRecords", 
-	      repeatitems: false, 
-	      id: "id" 
-	  },       
-	  pager: '#pagerFile', 
-	  rowNum:5, 
-	  rowList:[5,10,20,30,50], 
-	  sortname: 'name', 
-	  sortorder: 'asc', 
-	  viewrecords: true, 
-	  gridview: true, 
-	  height: 250, 
-	  width: 700, 
-	  onSelectRow: function(id){  
-	      //document.location.href ="${showFolderUrl}/" + id; 
-	  },
-	  loadComplete: function() {			    	
-	  }
-	});		
-	$( "#chooseFileDialog" ).dialog({
-          autoOpen: false,
-          height: 400,
-          width: 500,
-          modal: true,
-          resizable: false,
-          buttons:{
-        		'${labelActionSave}':function(){
-        			
-        		},
-        		'${labelActionClose}':function(){
-        			$("#chooseFileDialog").dialog("close");	
-        		}  
-          }
-    });
+	
 });	   
 function showChooseFileDialog(){	  
 	var dialog = $("#chooseFileDialog");
